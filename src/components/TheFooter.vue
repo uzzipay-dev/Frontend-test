f51494fed6c3dccb9f923613c45af62b2c765b10
souricardomaia <riicardolivera@gmail.com>
2021-06-12 15:33:38 -0400

    add(footer): rodapé

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <router-link to="/" class="titulo">
+      <p>Rick and Morty</p>
+    </router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+};
+</script>
+
+<style scoped>
+footer {
+  background: #fff;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: white;
+  position: relative;
+  top: 75px;
+}
+</style>
