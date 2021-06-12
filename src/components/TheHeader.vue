ab840f9145840653d2ca1d448a518e44b9d10bc8
souricardomaia <riicardolivera@gmail.com>
2021-06-12 15:33:58 -0400

    add(header): Cabeçalho.

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/" class="titulo">
+        <h1>Rick and Morty</h1>
+      </router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+
+<style scoped>
+header {
+  background: #fff;
+}
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+
+.titulo {
+  padding: 10px 0;
+}
+</style>
