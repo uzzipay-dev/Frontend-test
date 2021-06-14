b945e401eee15e97ac7a8538fee417c32e967b1e
mason <paulodanielaraujo.pd90@gmail.com>
2021-06-14 12:37:34 -0400

    upando

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './assets/css/App.css';
 import Home from "./pages/Home";
 import Footer from "./components/Footer/index";
+import server from "../server";
 
 
 function App() {
+  server;
   return (
     <>
       <Home />
